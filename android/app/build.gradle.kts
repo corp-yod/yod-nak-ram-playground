e10dcd3e088fce32e8593fde0be619411deba701
Tinnakorn Meejantan <tinnakorn.m@entronica.in.th>
2026-04-22 22:30:30 +0700

    fix: update Android build configuration and set namespace dynamically

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,8 +7,10 @@ plugins {
 
 android {
     namespace = "com.example.yod_nak_ram_app"
-    compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    compileSdk = 36
+    // compileSdk = flutter.compileSdkVersion
+    ndkVersion = "27.0.12077973"
+    // ndkVersion = flutter.ndkVersion
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -24,8 +26,11 @@ android {
         applicationId = "com.example.yod_nak_ram_app"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
+
+        minSdk = 24
+        // minSdk = flutter.minSdkVersion
+        targetSdk = 35
+        // targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -19,3 +19,24 @@ subprojects {
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
+
+subprojects {
+    // ใช้ plugins.withId เพื่อรอให้ Android Plugin โหลดเสร็จก่อนค่อยทำงาน
+    plugins.withId("com.android.application") {
+        configureAndroidNamespace(project)
+    }
+    plugins.withId("com.android.library") {
+        configureAndroidNamespace(project)
+    }
+}
+
+// ฟังก์ชันช่วยตั้งค่า namespace
+fun configureAndroidNamespace(project: Project) {
+    val android = project.extensions.findByName("android") as? com.android.build.gradle.BaseExtension
+    android?.apply {
+        if (namespace == null) {
+            // ถ้า group ว่าง ให้ใช้ชื่อโปรเจกต์แทน เพื่อป้องกัน error
+            namespace = if (project.group.toString().isNotEmpty()) project.group.toString() else "com.example.${project.name.replace("-", ".")}"
+        }
+    }
+}
